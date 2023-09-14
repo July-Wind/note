b7406e0e9a9049a6d115ffaca9aa046a0a4ab2e2
ShiYi <69226065+July-Wind@users.noreply.github.com>
2023-09-14 20:57:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,3 @@
-/*
- * @Author: N0ts
- * @Date: 2021-10-08 14:14:37
- * @LastEditTime: 2023-04-17 22:17:11
- * @Description: 配置
- * @FilePath: /eazy-gitee-note/js/config.js
- * @Mail：mail@n0ts.cn
- */
-
 export default {
     // Gitee 配置
     gitee: {
@@ -18,7 +9,7 @@ export default {
         sha: "Notes"
     },
     // 后端地址
-    serverBase: "https://api.n0ts.top/api/gitee",
+    serverBase: "",
     // 主题配置
     Themes: ["default-light", "default-dark"]
 };
