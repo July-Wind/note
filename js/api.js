ebdd74b3959a2a82bcdbead6ece42ad52d892692
ShiYi <69226065+July-Wind@users.noreply.github.com>
2023-09-14 20:55:58 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,12 +1,3 @@
-/*
- * @Author: N0ts
- * @Date: 2021-10-13 21:04:32
- * @LastEditTime: 2022-10-26 15:57:28
- * @Description: api 配置
- * @FilePath: /eazy-gitee-note/js/api.js
- * @Mail：mail@n0ts.cn
- */
-
 // 导入配置文件
 import config from "./config.js";
 
