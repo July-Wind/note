1fcd6860f014a35c9bd007255fcfc676a6519348
ShiYi <69226065+July-Wind@users.noreply.github.com>
2023-09-15 23:05:23 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
 // 导入配置文件
-import config from "./config.js";
+// import config from "./config.js";
 
 export default {
     // 获取文件数
-    getTree: `/repos/${config.gitee.owner}/${config.gitee.repo}/git/trees/${config.gitee.sha}?recursive=1`,
+    getTree: `https://gitee.com/api/v5/repos/shiyimao/note/git/trees/Notes?access_token=f6414c87abeb9ec4d54893b125cbb8de&recursive=1`,
     // 获取具体文件内容
-    getContent: `/repos/${config.gitee.owner}/${config.gitee.repo}/contents/`
+    getContent: `https://gitee.com/api/v5/repos/shiyimao/note/contents/`
 };
