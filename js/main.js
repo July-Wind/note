b9bbc5157ed1c0cc046ecd8e02d68e9af5654691
ShiYi <69226065+July-Wind@users.noreply.github.com>
2023-09-15 15:37:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@ const App = createApp({
             });
 
             // 转换为 html，超链接新建页面打开
-            this.content = marked(this.content).replaceAll("<a ", "<a target='_blank' ", "<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>");
+            this.content = marked(this.content).replaceAll("<a ", "<a target='_blank' ");
 
             // 转换后是否为空
             if (!this.content || this.content.trim() == "") {
